9c155d32802d48425bae69482be2b73809907019
Nicolai Reuschling <nicolai@reuschling.name>
2013-08-27 14:06:02 +0200

    gem scaffold from 'bundle gem'

---
+++ lib/ruby/genderize.rb
@@ -0,0 +1,7 @@
+require "ruby/genderize/version"
+
+module Ruby
+  module Genderize
+    # Your code goes here...
+  end
+end
---
+++ lib/ruby/genderize/version.rb
@@ -0,0 +1,5 @@
+module Ruby
+  module Genderize
+    VERSION = "0.0.1"
+  end
+end
